dee0bc1d9a06da132d4e2d5277fd4040ee2f3a92
Takuma HANATANI <mail@potato4d.me>
2020-12-24 12:58:35 +0900

    chore: Add HTML meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,14 +4,45 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'unlinkable',
+    title: 'unlinkable - potato4d.me',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: 'The easiest way to unlink an @mention.' },
+      { property: 'og:type', hid: 'og:type', content: 'website' },
+      { property: 'og:title', hid: 'og:title', content: 'unlinkable - potato4d.me' },
+      {
+        property: 'og:description',
+        hid: 'og:description',
+        content: 'The easiest way to unlink an @mention.',
+      },
+      { property: 'og:site_name', hid: 'og:site_name', content: 'unlinkable - potato4d.me' },
+      {
+        name: 'twitter:card',
+        hid: 'twitter:card',
+        content: 'summary_large_image',
+      },
+      {
+        property: 'og:url',
+        hid: 'og:url',
+        content: url,
+      },
+      { property: 'og:image', hid: 'og:image', content: 'https://i.gyazo.com/3ba3b7da6527f303734c656ca35dcdb5.png' },
+      {
+        property: 'og:image:secure_url',
+        hid: 'og:image:secure_url',
+        content: 'https://i.gyazo.com/3ba3b7da6527f303734c656ca35dcdb5.png',
+      },
+      {
+        name: 'twitter:description',
+        hid: 'twitter:description',
+        content: 'The easiest way to unlink an @mention.',
+      },
+      { name: 'twitter:title', hid: 'twitter:title', content: 'unlinkable - potato4d.me' },
+      { name: 'twitter:image', hid: 'twitter:image', content: 'https://i.gyazo.com/3ba3b7da6527f303734c656ca35dcdb5.png' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'canonical', href: 'https://unlinkable.potato4d.me' }
     ]
   },
 
